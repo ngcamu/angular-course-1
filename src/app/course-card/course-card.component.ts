1caf478660e239c582bb68058320262ff7cfd8ef
ngcamum <ngcamum@legal.regn.net>
2023-10-06 13:14:29 +0200

    first commit

---
+++ src/app/course-card/course-card.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input } from '@angular/core';
+import { Course } from '../model/course';
+
+@Component({
+  selector: 'course-card',
+  templateUrl: './course-card.component.html',
+  styleUrls: ['./course-card.component.css']
+})
+export class CourseCardComponent {
+  @Input({
+    required: true
+  })
+  course:Course;
+
+  constructor(){
+
+  }
+
+  ngOnInit(){
+
+  }
+}
